b0c6ae00759aa114a18b686e54705e288113d9af
SHLOK AGARWAL <139161753+Shri-Shlok-Agarwal@users.noreply.github.com>
2023-07-11 11:31:14 +0530

    Add files via upload

---
+++ MSA_D2.py
@@ -0,0 +1,98 @@
+"""a=5
+print(a)
+a=7
+print(a)
+print(type(a))"""
+
+#float new concept
+"""a=2e2
+b=2E2
+c=2e3
+print(a)
+print(b)
+print(c)
+print(type(a)"""
+
+#complex data type
+"""a=3+5j
+b=2-5.5j
+c=3+10.5j
+print(a)
+print(b)
+print(c)
+print(a+b )
+print(a+c)
+print(a>b)#error"""
+
+#boolean data type
+"""a=True
+b=False
+print(a)
+print(b)
+print(a+a)
+print(a+b)"""
+
+#none data type
+"""a=None
+print(a)
+print(type(a))"""
+
+#string data type
+"""name1="Shlok"
+name2='Agarwal'
+print(name1,name2)"""
+
+#Bytes data type
+"""a=[10,20,30]
+b=bytes(a)
+#b[1]=40 error immutable
+print(a[1])
+print(b[1])"""
+
+#Byte array function
+"""a=[10,20,30]
+b=bytearray(a)
+b[1]=40 #will run mutable
+print(a[1])
+print(b[1])"""
+
+#Creating List Using Range
+"""l=list(range(1,20))
+print(l)"""
+
+#Arithmetic Operator(+,-,*,/,**,//)
+#RelationshipOperator(>,<,==,>=,<=,!=)
+"""a=5
+b=9
+print(a!=b)"""
+
+#Logical Operatorx=6
+"""x=6
+y=5
+z=8
+print(z and x and y)
+print(z or x or y)
+print(z and x or y)
+print(z or x and y)
+print(x not and y)"""
+
+#Unary Operator(-a,+a)
+"""a=8
+print(a)
+print(-a)"""
+
+#Membership Operator(in,not in)
+"""L=["shlok","vishal","rana"]
+print("shlok" in L)
+print("Ankit" in L)
+print("vishal" in L)
+print("rana" not in L)"""
+
+#Identity Operator(is,not is)
+"""a=25
+b=25
+print(a is b)
+print(a is not b)
+print(id(a))
+print(id(b))"""
+
