b674ec479ed6e9b7bbc65184700f7ea337f30091
SHLOK AGARWAL <139161753+Shri-Shlok-Agarwal@users.noreply.github.com>
2023-07-22 00:36:59 +0530

    Strings.
    Strings.

--- MSA_D07.py
+++ MSA_D07.py
@@ -1,9 +1,10 @@
 #_________________________________________________________________________________________________________
-#DAY 7
-#_________________________________________________________________
+#DAY 07
+
+#6 |_____________________________________________________________
 #USING STRINGS
 
-#________________
+#6.1 |____________
 #Multiline string
 """a='''My name is shlok"
 b=my friend name is yash'
@@ -15,46 +16,53 @@ print(type(b))
 print(c)
 print(type(c))"""
 
-#_____________
+#6.2 |_________
 #Empty String
 """a=""
 print(a)
 print(type(a))"""
 
-#________________________
+#6.3 |____________________
 #ACCESSING USING INDEXING
 #Accessing a string with indexes in python
 """A="Hello world"
 print(A[0])
 print(A[3])"""
 
+#6.3.1 |___________
 #Accessing a string with indexes and range(slicing)
 """A="Hello world"
 print(A[1:6:1])
 print(A[4])"""
 
+#6.3.2 |___________
 #Accessing a string with out of bond indexes
 """A="Hello world"
 print(A[0])
 print(A[300])"""
 
+#6.3.3 |___________
 #Accessing a string with negative indexes
 """A="Hello world"
 print(A[-7])
 print(A[-4])"""
 
+#6.3.4 |___________
 #Accessing a string with negative indexes and range(slicing)
 """A="Hello world"
 print(A[-1:-6:-1])
 print(A[-4])"""
 
+#6.extras |______________
 #Print caracter by caracter by caracter using for loop
 """A="Hello world"
 for a in A:
     print(a)"""
 
-#________________________
+#6.4 |____________________
 #ACCESSING USING SLICING
+
+#6.4.1 |_____________
 #String operator using seperator case
 """s="Hello_world_of_python"
 print(s[::])
@@ -73,14 +81,14 @@ print(s[-15:-3:1])
 #et cetra
 """
 
-#______________________________
+#6.5 |__________________________
 #STRING IN PYTHON IS IMMUTABLE
 """name="lord puneet"
 print(name)
 print(name[0])
 name[0]='n'"""
 
-#_______________________________
+#6.6 |___________________________
 #Operators On Strings(+,*[only])
 """a="python"
 b="programing"
@@ -91,13 +99,13 @@ print(a*c)
 #print(a*b)
 print(type(a*c))"""
 
-#_______________________
+#6.7 |___________________
 #Length Of String(len())
 """a="Hello_World"
 print(a)
 print("Length of String is= ",len(a))"""
 
-#______________________________________________________
+#6.8 |__________________________________________________
 #Membership Operator in Python For STRINGS (in, not in)
 """a="Hello_World"
 print("o" in a)
@@ -106,7 +114,7 @@ print("a" not in a)
 print("orld" not in a)
 print("orld" in a)"""
 
-#_________________________________
+#6.9 |________________________________________
 #To Find Substring in Main String (in, not in)
 """main=input("Enter Main String: ")
 sub=input("Enter Sub String: ")
@@ -119,10 +127,10 @@ if sub not in main:
 else:
     print(sub,"is found in main string")"""
 
-#
+#6.10 |__________________________________________
 #Comparison Operators On Strings(<=,>=,<,>,==,!=)
 
-#eg
+#eg one | basics
 """s1="abcd"
 s2="abcdefg"
 print(s1==s2)
@@ -132,7 +140,7 @@ print(s1<s2)
 print(s1>s2)
 print(s1!=s2)"""
 
-#eg Check user name
+#eg two | Check user name
 """name="Shlok"
 user_name=input("Enter User Name: ")
 if user_name==name:
