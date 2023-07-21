6534e736eaee33dcecec42f76aa7199f3ad040a4
SHLOK AGARWAL <139161753+Shri-Shlok-Agarwal@users.noreply.github.com>
2023-07-22 00:24:52 +0530

    Patterns Using For Loop.
    Patterns Using For Loop.

--- MSA_D06.py
+++ MSA_D06.py
@@ -1,20 +1,24 @@
-#________________________________
+#5.2.1 |__________________________
 #To print pattern using for loop
+
 #Pattern one 
 """for i in range(1,6):
     for j in range (1,6):
         print(j,end=' ')
     print()"""
+
 #Pattern two
 """for i in range(1,6):
     for j in range (1,i+1):
         print(j,end=' ')
     print()"""
+
 #Pattern three
 """for i in range(1,6):
     for j in range (1,i+1):
         print(i,end=' ')
     print()"""
+
 #Pattern four
 """for i in range(1,7):
     for j in range (1,i+1):
@@ -24,16 +28,19 @@
         else:
             print(0,end=' ')
     print()"""
+
 #Pattern five
 """for i in range(1,6):
     for j in range (1,i+1):
         print("*",end=' ')
     print()"""
+
 #Pattern six
 """for i in range(5,0,-1):
     for j in range (i,0,-1):
         print("*",end=' ')
     print()"""
+
 #Pattern seven (imp)
 """for i in range(1,6):
     for j in range (1,6-i):
@@ -41,6 +48,7 @@
     for k in range (1,i+1):
         print("*",end=' ')
     print()"""
+
 #Pattern eight (imp)
 """for i in range(1,6):
     for j in range (1,6-i):
@@ -48,6 +56,7 @@
     for k in range (1,i+1):
         print("*  ",end=' ')
     print()"""
+
 #Pattern nine (imp)
 """for i in range(1,6):
     for j in range (1,6-i):
@@ -55,6 +64,7 @@
     for k in range (0,2*i-1):
         print("*",end=' ')
     print()"""
+
 #Pattern 10(imp)
 """for i in range(6):
     for j in range (6-i):
