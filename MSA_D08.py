76038f579b0839cfcaf2244eea0008977582f3bb
SHLOK AGARWAL <139161753+Shri-Shlok-Agarwal@users.noreply.github.com>
2023-07-22 00:48:28 +0530

    Strings_2.
    Strings_2.

--- MSA_D08.py
+++ MSA_D08.py
@@ -1,47 +1,50 @@
 #_____________________________________________________________________________________________________________________________________________________
-#DAY 8
+#DAY 08
 
-#____________________________________________________________________________________________________
+#6.11 |________________________________________________________________________________________________
 #PREDEFINED METHOD TO REMOVE SPACES FROM STRING
-#_____________________________________________________________________
+
+#6.11.1 |______________________________________________________________
 #Use Of STRIP Function(remove spaces from start and end of the string)
 """course=" Python "
 print("with spaces course lenth is: ",len(course))
 x=course.strip()
 print("after removing spaces course lenth is: ",len(x))"""
 
-#_____________________________________________________________
+#6.11.2 |______________________________________________________
 #Use Of LSTRIP Function(remove spaces from left of the string)
 """course=" Python"
 print("with spaces course lenth is: ",len(course))
 x=course.lstrip()
 print("after removing spaces course lenth is: ",len(x))"""
 
-#______________________________________________________________ 
+#6.11.3 |_______________________________________________________ 
 #Use Of RSTRIP Function(remove spaces from right of the string)
 """course=" Python "
 print("with spaces course lenth is: ",len(course))
 x=course.rstrip()
 print("after removing spaces course lenth is: ",len(x))"""
 
-#____________________________________________________________________________________________________
+#6.12 |_______________________________________________________________________________________________
 #TO FIND SUBSTRING IN A STRING (find,index,rfind,rindex)
-#____________________________________________
+
+#6.12.1 |_____________________________________
 #FORWARD Finding(find,index)
-#____________________
+
+#6.12.1.1 |___________
 #Use Of Find Function
 """course="Python Programming Language"
 print(course.find("g"))
 print(course.find("o",5,))
 print(course.find("zee",5,))#(if substring is not found then it will give -1 as result)"""
 
-#_____________________
+#6.12.1.2 |____________
 #Use Of Index Function
 """course="Python Programming Language"
 print(course.index("Programming"))
 print(course.index("hello"))#error(if substring is not found then it will give ValueError)"""
 
-#__________________
+#6.12.1.3 |_________
 #Handling the ERROR
 """s="Python Programming Language"
 try:
@@ -49,28 +52,29 @@ try:
 except ValueError:
     print("Not Found")"""
 
-#____________________________________________
+#6.12.2 |_____________________________________
 #BACKWARD Finding(rfind,rindex)
-#____________________
+
+#6.12.2.1 |___________
 #Use Of RFind Function
 """course="Python Programming Language"
 print(course.rfind("g"))
 print(course.rfind("o",5,))
 print(course.rfind("zee",5,))#(if substring is not found then it will give -1 as result)"""
 
-#_____________________
+#6.12.2.2 |____________
 #Use Of RIndex Function
 """course="Python Programming Language"
 print(course.rindex("Programming"))
 print(course.rindex("hello"))#error(if substring is not found then it will give ValueError)"""
 
-#__________________________________________________________________________________________________________
+#6.13 |_____________________________________________________________________________________________________
 #Use Of COUNT Function(it count the number of accurance of the substring)
 """course="Python Programming Language"
 print(course.count("Programming"))
 print(course.count("a"))"""
 
-#_________________________________________________________________________________________________________
+#6.14 |______________________________________________________________________________________________________
 #TO REPLACE A STRING WITH ANOTHER STRING WITH HELP OF REPLACE FUNCTION
 """s1="Python Programming Language"
 s2=s1.replace("Python","Java")
@@ -79,9 +83,10 @@ print(s2)
 print(id(s1))
 print(id(s2))"""
 
-#___________________________________________________________________________________________________________
+#6.15 |______________________________________________________________________________________________________
 #SPLITING A STRING IN PYTHON
-#___________
+
+#6.15.1 |_________
 #type 1
 """message="Python Programming Language"
 a=message.split()
@@ -91,7 +96,7 @@ print(type(message))
 print(type(a))
 for x in a:
       print(x)"""
-#___________
+#6.15.2 |__________
 #type 2
 """message="Python Programming Language,it is easy,undarstandable, and good"
 a=message.split(",")
@@ -99,10 +104,10 @@ print("Before Splitting: ",message)
 print("After Splitting: ",a)"""
 
 
-#______________________________________________________________________________________________________________________
+#6.16 |_______________________________________________________________________________________________________________
 #TO JOIN TWO STRINGS (join two strings with given seperator symbol)
 
-#_____________________________________________________________
+#6.16.1 |________________________________________________________
 #Seperato is '-' symbol joining strings by using JOIN() METHOD
 """profile=["Roshan","Actor","Bhartiya"]
 candidate='-'.join(profile)
@@ -111,7 +116,7 @@ print(candidate)
 print(type(profile))
 print(type(candidate))"""
 
-#_______________________________________________________________
+#6.16.2 |_________________________________________________________
 #Seperato is ': ' symbol joining strings by using JOIN() METHOD
 """profile=["Roshan","Actor","Bhartiya"]
 candidate=': '.join(profile)
@@ -120,7 +125,7 @@ print(candidate)
 print(type(profile))
 print(type(candidate))"""
 
-#________________________________________________________________________________________________
+#6.17 |_________________________________________________________________________________________
 #CHANGE CASE OF STRING IN PYTHON (upper,lower,swapcase,title,capitalize)
 """message="Python Programming Language,it is easy"
 print(message.upper())
@@ -129,7 +134,7 @@ print(message.swapcase())
 print(message.title())
 print(message.capitalize())"""
 
-#_________________________________________________________________________________________________________
+#6.18 |__________________________________________________________________________________________________
 #FORMATING STRINGS (it sets priority)
 """name="Rakesh"
 salary=100
@@ -138,7 +143,7 @@ print("{} 's salary is {} and his age is {}".format(name,salary,age))
 print("{0} 's salary is {1} and his age is {2}".format(name,salary,age))
 print("{x} 's salary is {y} and his age is {z}".format(y=age,z=salary,x=name))"""
 
-#_________________________________________________________________________________________________________
+#6.19 |___________________________________________________________________________________________________
 #CHARACTER DATA TYPE (always shows as string)
 """gender1="M"
 gender2="F"
