97dae37b579cf6d18299d82ca6b38065969c1090
SHLOK AGARWAL <139161753+Shri-Shlok-Agarwal@users.noreply.github.com>
2023-08-02 10:07:10 +0530

    MSA_D13(working of module).py

--- MSA_D13(working of module).py
+++ MSA_D13(working of module).py
@@ -1,7 +1,7 @@
 #___________________________________________________________________________________________
 #DAY 13 (working of module)
 
-import addmultiplication
+"""import addmultiplication
 print(addmultiplication.x)
 addmultiplication.sum(1,2)
-addmultiplication.multiplication(2,3)
+addmultiplication.multiplication(2,3)"""
